259a9661e0a189c786025a847cef9d89410a243f
Martin Backhaus <tinchoignacio33@gmail.com>
2025-09-29 18:32:57 -0300

    Create demoqa_tests.js

---
+++ demoqa_tests.js
@@ -0,0 +1,20 @@
+(function uiTest() {
+  console.log("Starting UI Test... ");
+  
+  const nameInput = document.querySelector("#userName");
+  const submitBtn = document.querySelector("#submit");
+  
+  if (!nameInput || !submitBtn) {
+    console.error("UI element not found!");
+    return;
+  }
+  
+  nameInput.value = "Martin QA";
+  submitBtn.click()
+  
+  const outputName = document.querySelector("#name);
+  if (outputName && outputName.innerText.includes("Martin QA")) {
+    console.log("UI Test passed: Name displayed correctly");
+  } else {
+    console.error("Name not found :(")
+  })();
